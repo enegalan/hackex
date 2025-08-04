641b89648f64522efcba847be5a8139f2b557006
Eneko Galan <enekogalanelorza@gmail.com>
2025-08-04 15:55:58 +0200

    Indent

--- resources/views/includes/modal.blade.php
+++ resources/views/includes/modal.blade.php
@@ -1,8 +1,8 @@
 <renderer class="modals-renderer">
     @foreach (glob(resource_path('views/includes/modals/*.blade.php')) as $modalPath)
-    @php
-        $modalName = basename($modalPath, '.blade.php');
-    @endphp
-    @include('includes.modals.' . $modalName)
-@endforeach
+        @php
+            $modalName = basename($modalPath, '.blade.php');
+        @endphp
+        @include('includes.modals.' . $modalName)
+    @endforeach
 </renderer>
